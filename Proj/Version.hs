4d31f84ad5a06966586127132321292e625c3fba
Tim Bauer <trbauer@gmail.com>
2015-02-14 15:34:12 -0800

    see prev

--- Proj/Version.hs
+++ Proj/Version.hs
@@ -1,3 +1,4 @@
 module Proj.Version where
 
-(vERSION,vERSION_DATE) = ("0.1.0.0","02/09/2015") -- initial revision
+-- (vERSION,vERSION_DATE) = ("0.1.0.0","02/09/2015") -- initial revision
+(vERSION,vERSION_DATE) = ("0.1.0.1","02/14/2015") -- added some verbose output
